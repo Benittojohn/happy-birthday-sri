43820ab54b4c8bfafeb9d43c64e9645dc1f9141b
Benittojohn <135183907+Benittojohn@users.noreply.github.com>
2023-09-11 20:08:33 +0530

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,10 @@
+function setLogin(){
+    const pass=document.getElementById("pass").value;
+    if(pass=="Queen7853"){
+        alert("This is that video")
+         window.location.href ="newpage.html";
+    }else
+    {
+        alert("This is not Sri");
+    }
+}
